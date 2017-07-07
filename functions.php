43e0408c9392379f38141feb8c3f8c4fb91fb344
Chad Danna <chad@formulated.io>
2017-07-06 22:50:54 -0600

    Hotfix: Disable REST Auth needs for testing reasons

--- functions.php
+++ functions.php
@@ -78,3 +78,11 @@ function custom_post_types() {
 }
 
 add_action( 'init', 'custom_post_types' );
+
+/*
+ * Automagically authorize every request
+ * INSECURE! DANGER! ONLY USE IN LOCAL ENVIRONMENT.
+ */
+add_filter( 'rest_authentication_errors', function(){
+    wp_set_current_user( 1 ); // replace with the ID of a WP user with the authorization you want
+}, 101 );
